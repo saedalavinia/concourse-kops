636197411944eb5f17e1c98aa0b1487c29f9912f
Saed <saedalav@gmail.com>
2018-07-26 14:44:01 -0400

    Adding scripts/deploy-k8s-cluster.sh script template

---
+++ concourse/scripts/deploy-k8s-cluster.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+[ "$VERBOSE" ] && set -x
+
+set -euo pipefail
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+echo "${PIBLIC_KEY}" >> "${THIS_DIR}/id_rsa.pub"
+cat ${THIS_DIR}/id_rsa.pub
